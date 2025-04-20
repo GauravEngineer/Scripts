fbf4f6314edf23d021ec31fed1092f959ca01b04
Gaurav <gauravkumar.eng@gmail.com>
2025-04-20 13:50:35 +0530

    Revert "addin scrips"
    This reverts commit b7a8bf8e6808a121f43a2d005b0db121bd590de5.

--- Docker_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install docker.io docker-compose -y
-sudo usermod -aG docker $USER
-
-
-
-#this is for user data when creating new instance
--- createDirectories.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for (( i=1;i<=$2;i++))
-do
-        mkdir $1-$i
-done
-ls
